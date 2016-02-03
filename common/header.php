350558af70e7def61206a2fe6d6e52015754d4fb
Christopher Lok <WindMage57@msn.com>
2016-02-03 22:40:21 +0000

    Remove PHPFreeChat

--- common/header.php
+++ common/header.php
@@ -21,7 +21,6 @@
     <!-- Custom styles for this template -->
     <link rel="stylesheet" href="/css/sidebar.css" type="text/css" />
     <link rel="stylesheet" href="/css/main.css" type="text/css" />
-    <link rel="stylesheet" type="text/css" href="/phpfreechat-2.1.0/client/themes/default/jquery.phpfreechat.min.css" />
     
     <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.ico" />
 </head>
