8ce96a672eeab39639bbf3e15d79c857985e66f6
Christopher Lok <WindMage57@msn.com>
2016-02-03 20:25:28 +0000

    Skype testing

--- common/header.php
+++ common/header.php
@@ -23,20 +23,10 @@
     <link rel="stylesheet" href="/css/main.css" type="text/css" />
     
     <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.ico" />
-    
-      <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="/js/jquery.min.js"><\/script>')</script>
-    <script src="/js/bootstrap.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="/js/ie10-viewport-bug-workaround.js"></script>
-    <script type="text/javascript" src="https://www.skypeassets.com/i/scom/js/skype-uri.js"></script>
 </head>
 
 <body>
-
+    <script type="text/javascript" src="https://www.skypeassets.com/i/scom/js/skype-uri.js"></script>
     <div id="page-wrap">
 
 
--- index.php
+++ index.php
@@ -12,17 +12,18 @@
         if(isset($_SESSION['LoggedIn']) && isset($_SESSION['Username']) && isset($_SESSION['Email']) && $_SESSION['LoggedIn']==1):
 ?>
             <h2>Under Construction...</h2>
-            <div id="BnSChat" style="width:20%;background-color:#0094ff"></div>
-            <script type="text/javascript">
-                Skype.ui({
-                    name: "chat",
-                    element: "BnSChat",
-                    participants: ["reginlaff","yeongwonhi"],
-                    listParticipants:true,
-                    imageSize: 32,
-                    imageColor: "white"
-                });
-            </script>
+            <div id="BnSChat" style="width:20%;background-color:#0094ff">
+                <script type="text/javascript">
+                    Skype.ui({
+                        name: "chat",
+                        element: "BnSChat",
+                        participants: ["reginlaff","yeongwonhi"],
+                        listParticipants:true,
+                        imageSize: 32,
+                        imageColor: "white"
+                    });
+                </script>
+            </div>
 <?php else: ?>
             <h2>Please Sign Up or Log In!</h2>
             <img src="https://scontent-lax3-1.xx.fbcdn.net/hphotos-xpl1/v/t1.0-9/12565404_10153196569315933_995639197534309339_n.jpg?oh=1d979ed1d96adb189b279c6c0c296388&oe=5735DC56" class="img-responsive" alt="Responsive image">
