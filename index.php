d374f4c5db7e071dfaa8d26d62bde1d16c64c3b0
Christopher Lok <WindMage57@msn.com>
2016-02-03 19:40:43 +0000

    Trying out Skype URIs.

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     $pageTitle = "Home";
     include_once "common/header.php"; 
 ?>
+<script src="https://www.skypeassets.com/i/scom/js/skype-uri.js"></script>
 <div id="main">
     <noscript>This site just doesn't work, period, without JavaScript</noscript>
     <div class="container">
