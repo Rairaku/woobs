9b2b0b700179b688c2eadaa7e88de73ee39cf5af
Christopher Lok <WindMage57@msn.com>
2016-02-03 20:09:24 +0000

    Remove Skype JS local

--- common/footer.php
+++ common/footer.php
@@ -9,7 +9,7 @@
     <script src="/js/bootstrap.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="/js/ie10-viewport-bug-workaround.js"></script>
-    <script src="/js/skype-uri.js"></script>
+    <script src="https://www.skypeassets.com/i/scom/js/*"></script>
 </body>
 
 </html>
